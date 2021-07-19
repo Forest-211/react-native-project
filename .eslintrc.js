51057479714a4f3ac8d0fb58b39ec9baecbed090
changlin93 <767425412@qq.com>
2021-07-19 11:35:35 +0800

    配置husky和lint-stage

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,4 +4,8 @@ module.exports = {
         '@react-native-community',
         'plugin:prettier/recommended', // 添加 prettier 插件
     ],
+    rules: {
+        'react-native/no-inline-styles': 0,
+        'no-sparse-arrays': 0,
+    },
 };
