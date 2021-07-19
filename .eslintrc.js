d1eba7c5f6d951b58fe2114d279a70fe98d44052
changlin93 <767425412@qq.com>
2021-07-19 11:17:14 +0800

    feat: 配置prettier、eslint、editorconfig规范

---
+++ .eslintrc.js
@@ -0,0 +1,7 @@
+module.exports = {
+    root: true,
+    extends: [
+        '@react-native-community',
+        'plugin:prettier/recommended', // 添加 prettier 插件
+    ],
+};
---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+    bracketSpacing: false,
+    jsxBracketSameLine: true,
+    singleQuote: true,
+    trailingComma: 'all',
+    arrowParens: 'avoid',
+    tabWidth: 4,
+    semi: true,
+    useTabs: false,
+    bracketSpacing: true,
+};
